58f4f079f7047b5182a4ecf1e2ca68c0beee134a
Jay <liuliangcheng@JaydeMacBook-Pro.local>
2024-11-03 17:30:24 -0500

    update

--- mylib/__init__.py
+++ mylib/__init__.py
@@ -1,4 +1,3 @@
-# __init__.py
-from .data_utils import load_data, filter_data
-from .stats_utils import calculate_mean, calculate_median
-from .visualization import plot_birth_trends
+from .data_utils import load_data, filter_data  # noqa: F401
+from .stats_utils import calculate_mean, calculate_median  # noqa: F401
+from .visualization import plot_birth_trends  # noqa: F401
