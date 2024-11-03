2e3b383cce2b258f60791fbf030e772cabc42839
Jay <liuliangcheng@JaydeMacBook-Pro.local>
2024-11-03 17:32:02 -0500

    update

--- mylib/__main__.py
+++ mylib/__main__.py
@@ -2,11 +2,10 @@ from mylib.data_utils import load_data
 from mylib.stats_utils import calculate_mean, calculate_median
 from mylib.visualization import plot_birth_trends
 
-
 def main():
     # Example usage of the functions in mylib
     data = load_data(
-        "/Users/liuliangcheng/Desktop/Duke/IDS_DE/jayliu_ids_de_week9/" 
+        "/Users/liuliangcheng/Desktop/Duke/IDS_DE/jayliu_ids_de_week9/"
         "US_birth.csv"
     )
     mean_births = calculate_mean(data, "births")
@@ -14,7 +13,6 @@ def main():
     print(f"Mean Births: {mean_births}")
     print(f"Median Births: {median_births}")
     plot_birth_trends(data)
-
 
 if __name__ == "__main__":
     main()
