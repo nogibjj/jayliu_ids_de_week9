e65368baa61e5276e0425199574fdd64ce35f73c
Jay <liuliangcheng@JaydeMacBook-Pro.local>
2024-11-04 00:15:11 -0500

    update

--- test_main.py
+++ test_main.py
@@ -1,5 +1,6 @@
 import subprocess
 import logging
+import papermill as pm
 
 logger = logging.getLogger(__name__)
 
@@ -20,3 +21,19 @@ def test_main_script_runs():
     assert (
         "Loading dataset..." in result.stderr
     ), "Script did not log 'Loading dataset...'"
+
+
+def test_colab_notebook_runs():
+    # Define the notebook URL and output path
+    notebook_url = (
+        "https://colab.research.google.com/drive/1_4-E6Qzfre5eQdrGY8RI7-TlfcrpFIh7"
+    )
+    output_path = "/tmp/output_notebook.ipynb"
+
+    try:
+        # Execute the notebook
+        pm.execute_notebook(notebook_url, output_path, parameters={})
+        logger.info("Google Colab notebook ran successfully")
+    except pm.PapermillExecutionError as e:
+        logger.error(f"Google Colab notebook encountered an error: {e}")
+        raise
